88dfba2f1b5f5ad00b9bfc4ea7ede5e5471b8f7a
Krisis69 <caqmdevelope@gmail.com>
2024-01-22 22:32:27 -0500

    Add enviroment for series

---
+++ src/app/enviroment/enviroment_series.ts
@@ -0,0 +1,29 @@
+import { IInterfaceSerie } from '../interfaces/interfaces';
+import { envRoot } from './enviroment';
+
+export const envSeries: IInterfaceSerie[] = [
+  {
+    url: `${envRoot.UrlBaseCuevana}/serie/la-casa-de-papel-berlin`,
+    color: '#07807a',
+  },
+  {
+    url: `${envRoot.UrlBaseCuevana}/serie/mis-aventuras-con-superman`,
+    color: '#B9331B',
+  },
+  {
+    url: `${envRoot.UrlBaseCuevana}/serie/akuma-kun`,
+    color: '#CE1D47',
+  },
+  {
+    url: `${envRoot.UrlBaseCuevana}/serie/el-monstruo-de-la-vieja-seul`,
+    color: '#AA7D2A',
+  },
+  {
+    url: `${envRoot.UrlBaseCuevana}/serie/marvel-daredevil`,
+    color: '#0A0603',
+  },
+  {
+    url: `${envRoot.UrlBaseNinja}/online/psycho-pass-110823/`,
+    color: '#099D9D',
+  },
+];
